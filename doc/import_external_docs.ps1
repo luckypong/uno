0073916dbb2b1f950c82a23569945106e15c9575
luckypong <11326846+luckypong@users.noreply.github.com>
2026-08-05 01:46:33 +0000

    chore: Update external docs commit hashes

--- doc/import_external_docs.ps1
+++ doc/import_external_docs.ps1
@@ -21,7 +21,7 @@ $external_docs = @{
     "uno.extensions"     = @{ ref="a96b52152fc557383dd397753f465bf7a516575c" }  #latest main commit
     "workshops"          = @{ ref="3515c29e03dea36cf2206d797d1bf9f8620370e3" }  #latest master commit
     "uno.samples"        = @{ ref="c994421e3bb0fbe61c8583e822a10a9bbfdef69e" }  #latest master commit
-    "uno.chefs"          = @{ ref="4bbc0569dc7ac0ddefe8b0de4be31beb3706a90b" }  #latest main commit
+    "uno.chefs"          = @{ ref="0bd2f167a1b87fc802433b051997ef125d22f4c9" }  #latest main commit
     "hd-docs"            = @{ ref="03a7394d7fdb548ec55d268bdd813c84930eb92e"; dest="studio/Hot Design" } #latest main commit
 }
 
